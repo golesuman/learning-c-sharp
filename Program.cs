46f3adabbd824b76a5b2bbe07e61140c3bebf66e
Suman Gole <suman.gole@truenary.com>
2022-11-15 21:20:47 +0545

    add: learning basics of C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace learning_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 10, secondNumber = 20;
+            Console.WriteLine("first number is {0} and second number is {1}", firstNumber, secondNumber);
+            int number = 10;
+            bool result = (number > 20) ? true : false;
+            Console.WriteLine(result);
+        }
+    }
+}
