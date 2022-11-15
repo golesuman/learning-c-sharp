eb81820c55ac09e92ce31c151551204a97044ebc
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-11-15 21:44:26 +0545

    Create for_each_loop.cs

---
+++ for_each_loop.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string[] names = {"Suman", "hari", "shyam", "sita", "karan"};
+        foreach(string name in names){
+            Console.WriteLine(name);
+        }
+    }
+}
